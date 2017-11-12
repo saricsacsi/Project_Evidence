6a598ca58b3c4a7199103476945dcb192f7cd914
saricsacsi <saricsacsi@users.noreply.github.com>
2017-11-12 23:38:58 +0100

    some new feather

--- Evidence_dev.sol
+++ Evidence_dev.sol
@@ -37,32 +37,64 @@ contract Ownable {
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 
-
-contract evidenceContract is Ownable{     
+contract evidenceContract is Ownable{  
+    using SafeMath for uint256;
+   
 
     uint256  public rate = 1;  // how much time we give for 1 wei or ether     
     address public myAddress = this;
     mapping (address => bool) public isMember;
+    address[] private userIndex;
+    uint256 public numberOfRecord = 0;
+    address public multiSig = 0xCD1d44dBdBFdc24C6E25E53282727557a7142f1D; // test multisig1  
+    
+  
 
-    event Newuser(address indexed who, uint256 indexed timelimit);
+    event Newuser(uint256 number, address indexed who, uint256 indexed timelimit);
+    event LogUpdateTimelimit(address indexed who, uint256 indexed timelimit);
 
     
 
   struct UserRecord {
-     
+      
       bytes32 md5sum; // hash md5sum to check the url sent by the user
       bytes32 _hash; // we get from the server/archivum, its also the name of the object in the storage
-      //uint256 timestamp; // when we get the datas
       uint256 timelimit; // when we want to hold the datas in the storage
   }
 
   struct Member {
     UserRecord userrecord;
-  }
+     }
 
   mapping(address => Member) members;
+  
  
 
   function addMember(address user_address, bytes32 md5sum, bytes32 _hash, uint timelimit) onlyOwner public returns(bool success) {
@@ -73,11 +105,9 @@ contract evidenceContract is Ownable{
     members[user_address].userrecord.md5sum = md5sum;
     members[user_address].userrecord._hash = _hash;
     members[user_address].userrecord.timelimit = timelimit;
-    isMember[user_address] = true;
-
-    Newuser(user_address,timelimit);
-
-    
+    isMember[user_address] = true;  
+    numberOfRecord++;
+    Newuser(numberOfRecord,user_address,timelimit);    
     return true;
   }
 
@@ -87,12 +117,43 @@ contract evidenceContract is Ownable{
     members[user_address].userrecord._hash,
     members[user_address].userrecord.timelimit );
   }   
+ function updateTimelimit(address user_address, uint timelimit) 
+    public
+    returns(bool success) 
+  {
+    require(isMember[user_address]); 
+    members[user_address].userrecord.timelimit = timelimit;
+    LogUpdateTimelimit(user_address,timelimit);
+    return true;
+  }
 
+  function getUserCount() 
+    public
+    constant
+    returns(uint count)
+  {
+    return userIndex.length;
+  }
+ 
 
-  function evidenceContract() public {             
+  function evidenceContract() public {  
+                  
     }    
 
-//  
+ function buy(address user_address, uint256 amount) onlyMember internal {
+   
+       // Calculate 
+    uint256 timeUnit = amount.mul(rate);  
+    uint256 newtimelimit = now.add(timeUnit);
+    updateTimelimit(user_address,newtimelimit);
+       
+    multiSig.transfer(this.balance); // better in case any other ether ends up here
+    }
+  
+  function () public payable {
+    buy(msg.sender, msg.value);
+ }
+ 
 
  modifier onlyMember() {
     require (isMember[msg.sender]);
--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 The basic status:  
  I want to hold the members and the datas in a record.
  Datas:
-	- address
+	- address - index
+
 	- hash1
 	- hash2
 	- timelimit
@@ -16,15 +17,24 @@ maybe more datas:
 
 I want to get back the datas from user ID. Update: I use the address to index of the record
 
+
 Plan: 
 - just the members can send ether to the contarct --> made onlyMember modifier
-- from the amount of ether i want to calculate the timelimit
-- I want to know an address is a member or not? --> isMember:true/false done
-- I want to host from the member a signal "OK" 
-- delete a record
-- know how many memebrs we have
-- maybe later change the timelimit 
 
+- from the amount of ether i want to calculate the timelimit --> ok ( the counting is wrong)
+
+- I want to know an address is a member or not? --> isMember:true/false done
+
+- I want to host from the member a signal "OK" 
+
+- delete a record -- > if we have status we dont need to delete
+
+- know how much memebrs we have --> I know how much record we have
+
+- maybe later change the timelimit --> updateTimelimit done
+
+- one address -more record , more hosted URL  --> back to one , 
+now the multiple record doeasnt work, so its need to do again
 
 
 
